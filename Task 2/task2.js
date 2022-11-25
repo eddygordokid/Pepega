263348164889e4d8405a82f95237069e1c5023e4
eddygordokid <dima120299@rambler.ru>
2022-11-25 23:30:38 +0300

    Update task2.js

--- Task 2/task2.js
+++ Task 2/task2.js
@@ -10,24 +10,28 @@ function inputNum(num) {
     } while (isNaN(isNum) || isNum <= null)
     return isNum;
 }
-function letSearch(num) {
+function letterSearch(num) {
     let engLetter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     let answ = '';
     let intОfNum = num;
-    let RemOfNum;
+    let remainderOfNum;
+    let i = 0;
     do {
-        if (Math.trunc(intОfNum) > 0) {
-            RemOfNum = Math.trunc((intОfNum - 1) % engLetter.length);
+            remainderOfNum = (intОfNum - 1) % engLetter.length;
             intОfNum = Math.trunc((intОfNum - 1) / engLetter.length);
-            answ = answ + engLetter.toString().charAt(RemOfNum);
-        }
-    } while (Math.trunc(intОfNum) > 0);
+            answ = answ + engLetter.toString().charAt(remainderOfNum);
+            i++;
+    } while (intОfNum > 0);
+    let tempLetterMas = Array.from(i);
+    tempLetterMas = answ.split('');
+    tempLetterMas = tempLetterMas.reverse()
+    answ = tempLetterMas.join('');
     return answ;
 }
 function main() {
-    let columnNumber = prompt('Номер столбца EXCELE ');
+    let columnNumber = prompt('Номер столбца Excel ');
     columnNumber = inputNum(columnNumber);
-    answ = letSearch(columnNumber);
-    document.write('Буквенное обозначение столбца в Excele = ' + answ);
+    answ = letterSearch(columnNumber);
+    document.write('Буквенное обозначение столбца в Excel = ' + answ);
 }
 main();
